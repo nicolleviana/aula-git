2a7198f780d263650e16c6701b13aa6c6a6c5fd2
nicolleviana <vianaigaracu@gmail.com>
2024-08-19 22:26:23 -0300

    elaboração de um programa- 19/08

---
+++ script.js
@@ -0,0 +1,14 @@
+/*
+Elabore um programa que:
+1. Solicite o nome do produto
+2. Solicite a quantidade vendida
+3. Solicite o valor do Produto.
+4. Apresente o valor Total
+5. Solicite a % de desconto
+6. Apresente o R$ total de desconto 
+7. Apresente o valor total da venda depois do desconto 
+*/
+
+var nomeProduto = prompt("Digite o nome do produto: ");
+var quantidadeVendida = parseInt(prompt("Digite a quantidade vendida: ") );
+var valorProduto = parseFloat(prompt(Digite o valor do produto: ));
